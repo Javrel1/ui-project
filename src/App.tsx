72231901b2dc2d36abde36a97cdbbefc076571b8
Epeaa <zavohirzumaev1@gmail.com>
2024-02-20 16:26:55 +0500

    upg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {Header} from "./Header/Header";
-import MainPage from "./Content/MainPage/MainPage";
 
 function App() {
   return (
     <div className="App">
         <Header/>
-        <MainPage/>
     </div>
   );
 }
