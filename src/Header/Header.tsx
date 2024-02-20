b63c7f60afc79a5243baace85c387bc0b5349176
Epeaa <zavohirzumaev1@gmail.com>
2024-02-20 16:49:50 +0500

    upg

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import style from './style.module.css';
-import logo from '../sources/logo.png';
-
 export function Header(){
 
     return(
         <div className={style.container}>
             <div className={style.header}>
-                {/*<div className={style.logo}>*/}
-                {/*    <img src={logo}/>*/}
-                {/*    <a>people</a>*/}
-                {/*</div>*/}
                 <div className={style.navigation}>
                     <a>Home</a>
                     <a>Products</a>
