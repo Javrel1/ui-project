9f5b6356c97e31b3aca346288007d6f1528e1baf
Epeaa <zavohirzumaev1@gmail.com>
2024-02-20 16:07:45 +0500

    upg

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import style from './style.module.css';
-import logo from '../sources/logo.png';
 
 export function Header(){
 
     return(
         <div className={style.container}>
+            {/*<div className={style.header}>*/}
+            {/*    /!*<div className={style.logo}>*!/*/}
+            {/*    /!*    <img src={logo}/>*!/*/}
+            {/*    /!*    <a>people</a>*!/*/}
+            {/*    /!*</div>*!/*/}
+            {/*    <div className={style.navigation}>*/}
+            {/*        <a>Home</a>*/}
+            {/*        <a>Products</a>*/}
+            {/*        <a>Support</a>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+
             <div className={style.header}>
-                <div className={style.logo}>
-                    <img src={logo}/>
-                    <a>people</a>
-                </div>
-                <div className={style.navigation}>
-                    <a>Home</a>
-                    <a>Products</a>
-                    <a>Support</a>
+                <a className={style.logo}>CompanyLogo</a>
+                <div className={style.header_right}>
+                    <a className={style.active}>Home</a>
+                    <a>Contact</a>
+                    <a>About</a>
                 </div>
             </div>
-            <div className={style.line}></div>
         </div>
     )
 }
