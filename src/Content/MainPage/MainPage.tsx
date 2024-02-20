d3df118cc37b4689efb59f0247a94d6b7eaae841
Epeaa <zavohirzumaev1@gmail.com>
2024-02-20 17:38:04 +0500

    upg

--- src/Content/MainPage/MainPage.tsx
+++ src/Content/MainPage/MainPage.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 import style from './style.module.css';
-import Box from "@mui/material/Box";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-
 function MainPage(){
     return(
-        <div className="container">
+        <div className={style.container}>
                 <div className={style.main}>
                     <div className={style.we_can}>
                         <a>We will help you find your</a>
-                        <a>place in Programming!</a>
+                        <a>place in Programming</a>
+                        <a>and make a career!</a>
                     </div>
-                    <div className={style.bigText}>
-                        <a>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid ducimus</a>
-                        <a>illo, inventore omnis quo recusandae sed sit</a>
-                        <a>tenetur voluptate voluptatum.</a>
+                    <div className={style.getStarted}>
+                        <button>Let's Go</button>
                     </div>
                 </div>
         </div>
