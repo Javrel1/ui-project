179d4a7cc4f423d954826d4d403464076ce9fae9
Epeaa <zavohirzumaev1@gmail.com>
2024-02-24 13:53:33 +0500

    upg

--- src/Content/Registration/Registration.tsx
+++ src/Content/Registration/Registration.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import style from './style.module.css';
-import team from '../../sources/team.jpg';
 
 export function Registration(){
     return(
         <div className={style.container}>
             <div className={style.main}>
-                <div className={style.text}>
                     <div className={style.title}>
                         <a>Leave your details for contact</a>
                     </div>
-                </div>
                 <div className={style.forms}>
                     <div className={style.input}>
                         <input placeholder={"First Name"} type="text"/>
